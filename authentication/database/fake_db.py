9cc3de69d43791d1561b9013c5d067c95228b745
iamPMV259 <minhvu2592005@gmail.com>
2025-12-24 17:31:50 +0700

    filter

--- authentication/database/fake_db.py
+++
@@ -1,7 +0,0 @@
-from schemas.auth_schema import UserRole
-from core.utils import hash_pw
-
-db = {
-    "NKLHAHA": {'id': 1, "username": "NKLHAHA", "password": hash_pw("Moitao"), "role": UserRole.ADMIN}, 
-    "LNGHAHA": {'id': 2, "username": "LNGHAHA", "password": hash_pw("Moitao"), "role": UserRole.CAN_BO_PHUONG}
-}
--- authentication/main.py
+++
@@ -1,8 +0,0 @@
-from api.routes_auth import auth_router
-from fastapi import FastAPI
-# from database.supabase_client import supabase_client
-
-app = FastAPI()
-app.include_router(auth_router, prefix='/api/v1/auth', tags = ['Authentication'])
-
-
